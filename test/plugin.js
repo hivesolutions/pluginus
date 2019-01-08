6717a09d5aa7e91af86f6df2b570e3e11481d7cf
joamag <joamag@hive.pt>
2019-01-08 18:53:25 +0000

    Better ensure of plugin loading

--- test/plugin.js
+++ test/plugin.js
@@ -6,7 +6,11 @@ describe("Plugin", function() {
         it("should create a new Plugin instance", async () => {
             const plugin = pluginus.Plugin.new();
             assert.strictEqual(plugin instanceof pluginus.Plugin, true);
+            assert.strictEqual(plugin.name, "Plugin");
+            assert.strictEqual(plugin.version, "0.0.0");
             assert.strictEqual(plugin.owner, undefined);
+            assert.strictEqual(plugin.getName(), "Plugin");
+            assert.strictEqual(plugin.getVersion(), "0.0.0");
         });
     });
 });
