5bca500f511445e1f33f19a5d809c549a07add38
joamag <joamag@gmail.com>
2019-01-08 15:03:41 +0000

    Better code

--- test/simple.js
+++ test/simple.js
@@ -1,13 +1,12 @@
 const assert = require("assert");
-const lib = require("../lib");
 
-describe("Array", function () {
-    describe("#indexOf()", function () {
+describe("Array", function() {
+    describe("#indexOf()", function() {
         it("should return -1 when the value is not present", () => {
             assert.strictEqual(-1, [1, 2, 3].indexOf(4));
         });
     });
-    describe("#length", function () {
+    describe("#length", function() {
         it("should return proper length", () => {
             assert.strictEqual(3, [1, 2, 3].length);
         });
