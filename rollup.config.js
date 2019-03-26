6544f949e2261fdd3363875c3603bb2a8b2555f1
joamag <joamag@gmail.com>
2019-03-26 17:35:14 +0000

    New babel export of pluginus

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import pkg from "./package.json";
@@ -25,7 +26,11 @@ export default [
         },
         plugins: [
             resolve(),
-            commonjs()
+            commonjs(),
+            babel({
+                babelrc: false,
+                presets: ["@babel/preset-env"]
+            })
         ]
     },
     {
