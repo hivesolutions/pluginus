d07596843ca185958df464c7c476a08dff24ac30
joamag <joamag@gmail.com>
2019-04-12 14:12:28 +0100

    Better unit tests

--- test/plugin.js
+++ test/plugin.js
@@ -1,6 +1,10 @@
 const assert = require("assert");
 const pluginus = require("..");
 
+beforeEach(async function() {
+    pluginus.reset();
+});
+
 describe("Plugin", function() {
     describe("#new()", function() {
         it("should create a new Plugin instance", async () => {
@@ -14,14 +18,20 @@ describe("Plugin", function() {
         });
 
         it("should register itself on the global manager instance", async () => {
+            assert.strictEqual(pluginus.manager.plugins.length, 0);
+
             pluginus.Plugin.register();
-            assert.strictEqual(pluginus.manager.plugins["Plugin"] !== null, true);
+            assert.strictEqual(pluginus.manager.plugins.length, 1);
+            assert.strictEqual(pluginus.manager.plugins[0].name, "Plugin");
         });
 
         it("should register itself to the provided manager instance", async () => {
             const manager = new pluginus.PluginManager();
+            assert.strictEqual(manager.plugins.length, 0);
+
             pluginus.Plugin.register(manager);
-            assert.strictEqual(manager.plugins["Plugin"] !== null, true);
+            assert.strictEqual(manager.plugins.length, 1);
+            assert.strictEqual(manager.plugins[0].name, "Plugin");
         });
     });
 });
