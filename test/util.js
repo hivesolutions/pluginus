c7ccf0f2dd2d3e77fac538543d8bc916e718b510
joamag <joamag@hive.pt>
2020-04-12 18:19:00 +0100

    Small camel to undescore change

--- test/util.js
+++ test/util.js
@@ -1,6 +1,23 @@
 const assert = require("assert");
 const pluginus = require("..");
 
+describe("#camelToUnderscore()", function() {
+    it("should be able to convert simple values", () => {
+        let result;
+
+        result = pluginus.camelToUnderscore(null);
+        assert.strictEqual(result, null);
+
+        result = pluginus.camelToUnderscore("");
+        assert.strictEqual(typeof result, "string");
+        assert.strictEqual(result, "");
+
+        result = pluginus.camelToUnderscore("HelloWorld");
+        assert.strictEqual(typeof result, "string");
+        assert.strictEqual(result, "hello_world");
+    });
+});
+
 describe("#normalizeName()", function() {
     it("should normalize names in a proper fashion", async () => {
         assert.strictEqual(pluginus.normalizeName("Plugin"), "plugin");
